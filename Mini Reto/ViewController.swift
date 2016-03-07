23d80a8ec53d99c77c16ac5c196ad9708d95eb11
Hector Avila Loyer <Hector@macbook-air-de-hector.local>
2016-03-06 20:52:43 -0500

    Initial Commit

---
+++ Mini Reto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini Reto
+//
+//  Created by Hector Avila Loyer on 6/3/16.
+//  Copyright © 2016 Hector Avila Loyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
